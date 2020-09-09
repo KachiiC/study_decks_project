3cc7397cefc0464bda531343d3c253d52c162dbf
Kachii <Kacheokcheong@gmail.com>
2020-09-09 17:19:01 +0100

    second version

--- deck_frontend/src/Pages/StudyArea/index.js
+++ deck_frontend/src/Pages/StudyArea/index.js
@@ -3,17 +3,19 @@ import React, { useState, useEffect } from "react"
 function StudyArea () {
 
     const [isFetching, setIsFetching] = useState(true)
-    const [word, setWord] = useState(1)
+    const [word, setWord] = useState(0)
     const [displayWord, setDisplayWord] = useState([])
     const [isDisplayable, setIsDisplayable] = useState(false)
+    const [showResults, setShowResults] = React.useState(false)
 
     useEffect(() => {
-        fetch(`http://127.0.0.1:8000/study_deck/${word}`)
+        fetch('http://127.0.0.1:8000/study_deck_list')
         .then((response) => { 
             return response.json() 
         })
         .then((wordData) => { 
             setDisplayWord(wordData)
+            setShowResults(false)
             setIsDisplayable(true)
             setIsFetching(false)
         })
@@ -23,34 +25,38 @@ function StudyArea () {
         
     }, [word])
 
-    function nextWord() {
-        setWord(prevWord => prevWord + 1)
-    }
+    const showMe = () => setShowResults(true)
 
-    function lastWord() {
-        setWord(prevWord => prevWord - 1)
-    }
-
-
+    const Results = () => (
+        <div>
+            <p><b>Definition:</b> {displayWord[word].definition}</p>
+            <p><b>Romanization:</b> {displayWord[word].romanization}</p>
+            <p><b>Notes:</b> {displayWord[word].notes}</p>
+            <p>{displayWord[word].notes}</p>
+        </div>
+    )
 
     const renderLogic = (isFetching)?(
         <div>Loading</div>
     ):((isDisplayable)?(
         <div>
-            <p>{displayWord.word_phrase}</p>
-            <p>{displayWord.notes}</p>
+            <h1>{displayWord[word].word_phrase}</h1>
+            { showResults ? <Results /> : null }
         </div>
         
         ):(<div>Unable to display</div>))
 
-    
-
     return (
         <>
+        <div className="home-container">
             <h2>Study Area</h2>
             {renderLogic}
-            <button onClick={lastWord}>previous word</button>
-            <button onClick={nextWord}>next word</button>
+            <div>
+                <button onClick={() => {setWord(prevWord => prevWord + 1)}}>previous word</button>
+                <button onClick={showMe}>Show Answer</button>
+                <button onClick={() => {setWord(prevWord => prevWord + 1)}}>next word</button>
+            </div>
+        </div>
         </>
     )
 }
